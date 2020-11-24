9fddbb8d53b4e79c21ff5feea1d951b4a387ad46
Megan Cassidy <61455706+MeganCassidy@users.noreply.github.com>
2020-11-24 20:18:16 +0000

    javascript update
    functionality added for the countdown clock, calculates difference between current time and the future date

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
 let clockH = document.querySelector('.clock__hours');
 let clockM = document.querySelector('.clock__minutes');
 let clockS = document.querySelector('.clock__seconds');
+let second = 1000;
+let minute = second * 60;
+let hour = minute *60;
+let day = hour * 24;
+let finalDate = new Date('December 25, 2021 00:00:00');
 
 let startClock = () => {
     updateTime();
@@ -16,6 +21,21 @@ let updateTime = () => {
     clockH.style.transform = `rotate(${360 / 12 * hours}deg)`;
     clockM.style.transform = `rotate(${360 / 60 * minutes}deg)`;
     clockS.style.transform = `rotate(${360 / 60 * seconds}deg)`;
+}
+
+let updateCountdown = () => {
+    let now = new Date(); 
+    let diff = finalDate - now;
+    console.log(convertMillisToDHMS(diff));
+}
+
+let convertMillisToDHMS = (millis) => {
+    let days = Math.floor(millis / day);
+    let hours = Math.floor(millis % day / hour);
+    let minutes = Math.floor(millis % hour / minute);
+    let seconds = Math.floor(millis % minute / second);
+    return {days, hours, minutes, seconds};
+
 }
 
 startClock();
